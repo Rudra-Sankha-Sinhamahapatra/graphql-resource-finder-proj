a853eee1e3853deae1105902f1913fecc7bc4338
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2025-05-31 00:58:46 +0530

    fix

--- client/src/layouts/MainLayout.tsx
+++ client/src/layouts/MainLayout.tsx
@@ -141,7 +141,7 @@ export default function MainLayout() {
         <div className="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
           <div className="mt-8 md:order-1 md:mt-0">
             <p className="text-center text-xs leading-5 text-gray-500">
-              &copy; 2024 DevLink. All rights reserved.
+              &copy; 2025 DevLink. All rights reserved.
             </p>
           </div>
         </div>
